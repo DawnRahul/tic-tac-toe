9406d7cce026914f903c4eeb8e130187b6dddc60
Rahul Kumar <45137578+DawnRahul@users.noreply.github.com>
2023-03-28 15:44:16 +0530

    js_update_ver_0.2
    Add feature of tracking score and replay

--- app.js
+++ app.js
@@ -1,86 +1,147 @@
 const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.querySelector("#info");
 
+let player_1_Score = 0;
+let player_2_Score = 0;
+let draw = false;
+let attempts = 0;
+
+const cells = ["", "", "", "", "", "", "", "", ""];
+
+infoDisplay.textContent = "Circle go first!";
 let go = "circle";
-infoDisplay.textContent = "Circle goes first!"
 
-const startCells = ["", "", "", "", "", "", "", "", ""];
+const box = document.createElement('div');
+box.classList.add('square');
 
-//Creating Board
+cells.forEach((_cell, index) => {
+    const box = document.createElement('div');
+    box.classList.add('square');
+    box.id = index;
 
-function createBoard() {
-    startCells.forEach((_cell, index) => {
-        const cellElement = document.createElement('div');
-        cellElement.classList.add("square");
-        cellElement.id = index;
-        //append squares inside game board
-        gameBoard.append(cellElement);
+    gameBoard.append(box);
 
-        //on click
-        cellElement.addEventListener('click', addGo);
-    });
-}
+    box.addEventListener('click', addGo);
+});
 
-//call the create board function for boxes to come on board
-createBoard();
-
-function addGo(e) {
-    // console.log(e.target);
+//Function to add circle or cross into the box by player
+function addGo(event){
+    // console.log(event.target);
     const displayGo = document.createElement('div');
     displayGo.classList.add(go);
 
-    e.target.append(displayGo);
+    event.target.append(displayGo);
+    event.target.removeEventListener('click', addGo);
 
-    //player change
     go = go === "circle" ? "cross" : "circle";
+    infoDisplay.textContent = "It's now " + go +"'s turn!";
 
-    infoDisplay.textContent = `Now it's ${go}'s go!`;
-    //removing event listerner from marked box
-    e.target.removeEventListener("click", addGo);
+    checkWin();
 
-    //cehcking score
-    checkScore();
+    let player_1 = document.querySelector('#pl-1-score');
+    let player_2 = document.querySelector('#pl-2-score');
+
+    player_1.innerHTML = player_1_Score;
+    player_2.innerHTML = player_2_Score;
 }
 
-function checkScore() {
-    const winingCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
-    ];
+
+//Funcion to check Win chance for players after each go(turn or mark)
+function checkWin(){
+    const winingCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     const allSquares = document.querySelectorAll(".square");
-    // console.log(allSquares);
 
-    //Everytime check all wining combos 
-    winingCombos.forEach((comboArray) => {
-        const circleWins = comboArray.every((cellNumber) => {
-            return allSquares[cellNumber].firstChild?.classList.contains('circle');
+
+//Check win chance for player with "Circle"
+    winingCombos.forEach((comboArr) => {
+        const wins = comboArr.every((cellNumber) => {
+            return allSquares[cellNumber].firstElementChild?.classList.contains('circle');
         });
 
-        console.log(circleWins);
-        //comboArray.every() returns boolean true or false
-        if (circleWins) {
-            infoDisplay.textContent = "Circle Wins!";
-            allSquares.forEach((square) => { square.replaceWith(square.cloneNode(true)) });
-            return;
+        // console.log("circle: " + wins);
+
+        if(wins) {
+            infoDisplay.textContent = "Circle Wins!"
+            allSquares.forEach((square)=>{
+                square.removeEventListener("click", addGo);
+            });
+
+            attempts = 0; //reseting attempts value to 0;
+            player_1_Score++;
         }
 
 
     });
 
-
-    //for Cross Win's check
-    winingCombos.forEach((comboArray) => {
-        const crossWins = comboArray.every((cellNumber) => {
-            return allSquares[cellNumber].firstChild?.classList.contains("cross");
-        });
-
-        console.log(crossWins);
-        //comboArray.every() returns boolean true or false
-        if (crossWins) {
-            infoDisplay.textContent = "Cross Wins!";
-            allSquares.forEach((square) => { square.replaceWith(square.cloneNode(true)) });
-            return;
-        }
-
-
+//Check win chance for player with "Cross"
+winingCombos.forEach((comboArr) => {
+    const wins = comboArr.every((cellNumber) => {
+        return allSquares[cellNumber].firstElementChild?.classList.contains('cross');
     });
+
+    // console.log("crosss: " + wins);
+
+    if(wins) {
+        infoDisplay.textContent = "Cross Wins!"
+        allSquares.forEach((square)=>{
+            square.removeEventListener("click", addGo);
+        });
+        attempts = 0; //reseting attempts value to 0;
+        player_2_Score++;
+    }
+
+});
+
+//tracking number of attemts
+attempts++;
+//if 9 attemts done tand still no one won, then its a draw
+if(attempts == 9){
+    draw = true;
+    infoDisplay.textContent = "DRAW!!";
 }
+
+
+}//checkWin()
+
+
+
+//***************************REPLAY************************
+
+/*
+task to do:
+provide a button to replay - 
+remove all the classes from all boxes
+start from giving player chance to mark again - adding Go class
+
+*/
+
+const replay = document.querySelector(".btn-replay");
+
+replay.addEventListener("click", replayGame);
+
+function replayGame(){
+    const allSquares = document.querySelectorAll(".square");
+
+    for(let i = 0; i<9 ; i++){
+
+        const square = gameBoard.querySelectorAll(".square")[i];
+
+        //checking if square is already marked, before removing marks(circle or cross)
+        while (square.hasChildNodes()) {
+            square.removeChild(square.firstChild);
+          }
+    }
+
+        //adding back the event listener to the square
+        allSquares.forEach((square)=>{
+            square.addEventListener("click", addGo);
+        });
+
+    go = "circle";
+    infoDisplay.textContent = "Circle go first!" ; 
+    attempts = 0; //reseting attempts value to 0;
+
+}
+
+
+
